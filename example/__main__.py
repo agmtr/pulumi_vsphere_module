d61813f78dc89f0ed2b17a43b917533e3b50198f
A Bob <agmtr@MacBook-Air-A.local>
2023-10-18 13:54:56 +0400

    Initial commit

--- example/__main__.py
+++ example/__main__.py
@@ -7,10 +7,16 @@ Instance(
     args=InstanceArgs(
         cpus=2,
         memory=2048,
+        disks={
+            "disk0": 20,
+            "disk1": 30
+        },
         networks=[
             NetworkArgs(
                 ip_address="10.102.11.200"
             )
-        ]
+        ],
+        template="rocky-9-template",
+        userdata_file="userdata.yaml"
     )
 )
